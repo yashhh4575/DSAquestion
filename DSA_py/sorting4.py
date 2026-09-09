7df7cadc9138b4ff4d7f508e8ebd94f2512942ee
yash <thakuryashsingh0007@gmail.com>
2026-09-09 19:11:43 +0530

    updateed sort code

--- DSA_py/sorting4.py
+++ DSA_py/sorting4.py
@@ -10,4 +10,13 @@ def section_sort(nums):
                 mini_ind = j
         nums[i],nums[mini_ind]=nums[mini_ind],nums[i]  
 section_sort(nums)    
-print(nums)                 
+print(nums)
+
+# bubble sorting 
+nums = [3,2,4,5,6,3,1,8,10]
+n = len(nums)
+for i in range(n-2,-1,-1):
+    for j in range(0,i+1):
+        if nums[j]>nums[j+1]:
+            nums[j],nums[j+1]=nums[j+1],nums[j]
+print(nums)            
