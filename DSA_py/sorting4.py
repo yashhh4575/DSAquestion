df57fcce6a002f6f06c351ec4f76556f4374a5f3
yash <thakuryashsingh0007@gmail.com>
2026-09-10 18:56:20 +0530

    new sorted file

--- DSA_py/sorting4.py
+++ DSA_py/sorting4.py
@@ -33,4 +33,35 @@ for i in range(1,n):
         nums[j+1]=nums[j]
         j-=1
         nums[j+1]= key
-print(nums)        
+print(nums)       
+
+# merge two sorted array 
+
+left = [ 1,2,3,4]
+right = [1,1,2,3,4,5,6,7]
+
+def merge_array(left,right):
+    result = []
+    i, j = 0,0
+    n, m = len(left),len(right)
+    while i<n and j<m:
+        if left[i]<=right[j]:
+         result.append(left[i])
+         i += 1
+        else:
+            result.append(right[j])
+            j += 1
+        if i<n:
+            while i<n:
+                result.append(left[i])
+                i += 1
+        if j<m:
+            while j<m:
+                result.append(right[j])
+                j += 1
+        return result 
+print(merge_array(left,right))                               
+
+
+    
+    
