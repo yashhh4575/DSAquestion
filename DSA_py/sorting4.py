1c3baa7218eed9258876391b81300fe34b59564f
yash <thakuryashsingh0007@gmail.com>
2026-09-10 18:00:23 +0530

    new updated file

--- DSA_py/sorting4.py
+++ DSA_py/sorting4.py
@@ -20,3 +20,17 @@ for i in range(n-2,-1,-1):
         if nums[j]>nums[j+1]:
             nums[j],nums[j+1]=nums[j+1],nums[j]
 print(nums)            
+
+# insertion sort 
+
+nums = [3,5,6,4,8,9,10,7,1]
+
+n = len(nums)
+for i in range(1,n):
+    key = nums[i]
+    j=i-1
+    while j>=0 and nums[j]>key:
+        nums[j+1]=nums[j]
+        j-=1
+        nums[j+1]= key
+print(nums)        
