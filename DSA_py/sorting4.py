63bddd6db175c499ec42f7a0825e5feee8de1fec
yash <thakuryashsingh0007@gmail.com>
2026-09-11 19:12:49 +0530

    new sorted file

--- DSA_py/sorting4.py
+++ DSA_py/sorting4.py
@@ -80,4 +80,32 @@ def merge_sort(arr):
 
     return merge_array(left,right) 
 print(merge_sort(nums)) 
+
+# quick sort 
+
+nums = [4,1,7,6,3,2,8]
+
+def partition(nums, low, high):
+    pivot = nums[low]
+    i = low 
+    j = high
+    while i<j:
+        while nums[i]<=pivot and i<= high -1:
+            i+=1
+        while nums[j]>pivot and j>=low+1:
+            j-=1
+        if i<j:
+            nums[i],nums[j]= nums[j],nums[i]
+        nums[j],nums[low]=nums[low],nums[j]
+        return j        
+
+def quick_sort(nums,low,high):
+    if low<high:
+     p_ind = partition(nums,low,high)
+     quick_sort (nums,low, 
+                p_ind-1)
+     quick_sort (nums,p_ind+1,
+                high)
+quick_sort(nums,0,len(nums)-1)
+print(nums)
     
