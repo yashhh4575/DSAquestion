22be31d356a20594b1652e21791fe2bc2877c004
yash <thakuryashsingh0007@gmail.com>
2026-09-11 22:10:26 +0530

    new array file

--- DSA_py/array.py
+++ DSA_py/array.py
@@ -5,7 +5,7 @@
 
 nums = [22,34,55,99,1,2,4]
 
-largest = nums[0]
+largest = nums[0] #largest = float("-inf") it is represent - infinite
 n = len(nums)
 for i in range(0,n):
     if nums[i]>=largest:
