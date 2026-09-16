670532f035797e605d037e01e847d15e5c12e63b
yash <thakuryashsingh0007@gmail.com>
2026-09-16 22:18:43 +0530

    new optimal file

--- DSA_py/array.py
+++ DSA_py/array.py
@@ -37,8 +37,22 @@ for i in range(len(nums)):
 
 print(s_largest)
 
+# optimal sol.
+nums = [55, 32, 97, -55, 45, 32, 88, 21]
 
+largest = float("-inf")
+s_largest = float("-inf")
 
+for i in range(0, len(nums)):
+
+    if nums[i] > largest:
+        s_largest = largest
+        largest = nums[i]
+
+    elif nums[i] > s_largest and nums[i] != largest:
+        s_largest = nums[i]
+
+print(s_largest)
 
 
 
