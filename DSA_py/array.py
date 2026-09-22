4ccc138d2d14d1c32c960473aa4d755cf0254321
yash <thakuryashsingh0007@gmail.com>
2026-09-22 18:15:53 +0530

    new yash file

--- DSA_py/array.py
+++ DSA_py/array.py
@@ -211,4 +211,45 @@ for i in range(0, n):
         break
 else:
     print(-1)
+
+
+# marge 2 sorted array 
+
+nums1 = [1, 1, 1, 2, 4, 6, 7]
+nums2 = [1, 2, 3, 6, 7, 8, 9, 10]
+
+n = len(nums1)
+m = len(nums2)
+result = []
+
+i = 0
+j = 0
+
+while i < n and j < m:
+
+    if nums1[i] == nums2[j]:
+
+        if len(result) == 0 or result[-1] != nums1[i]:
+            result.append(nums1[i])
+
+        i += 1
+        j += 1
+
+    elif nums1[i] < nums2[j]:
+        i += 1
+
+    else:
+        j += 1
+
+while i < n:
+    if len(result) == 0 or result[-1] != nums1[i]:
+        result.append(nums1[i])
+    i += 1
+
+while j < m:
+    if len(result) == 0 or result[-1] != nums2[j]:
+        result.append(nums2[j])
+    j += 1
+
+print(result)    
     
